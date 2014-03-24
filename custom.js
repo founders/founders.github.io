5e0bece8380a0e8fdd24dd925b7b051f8484faf6
Rohan Kapoor <rohan@rohankapoor.com>
2014-03-24 02:44:39 -0400

    Modifying people for titles to appear as overlays

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 function show(id) {
-	document.getElementById(id).style.display = "block";
+	document.getElementById(id).style.visibility = "visible";
 }
 
 function hide(id) {
-	document.getElementById(id).style.display = "none";
+	document.getElementById(id).style.visibility = "hidden";
 }
